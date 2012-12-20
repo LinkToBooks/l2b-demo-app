540f6b1dc114413afb4094d273feda3a80379da6
Edmund von der Burg <evdb@mysociety.org>
2012-12-20 20:57:10 +0000

    Serve 404 page for anything non-static

--- index.js
+++ index.js
@@ -10,5 +10,10 @@ module.exports = function () {
 
   app.use( '/', express.static( publicDir ) );
 
+  app.all( '*', function (req,res) {
+    res.status(404);
+    res.sendfile( publicDir + '/404.html' );
+  });
+
   return app;
-}
+};
