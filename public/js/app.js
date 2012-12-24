3dd235f975398917878c1c2d23ada8d062ee45db
Edmund von der Burg <evdb@mysociety.org>
2012-12-24 10:23:54 +0000

    Added simple router for isbn numbers

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,15 @@
+var Router = Backbone.Router.extend({
+  routes: {
+    'isbn/:isbn': 'isbnDisplay',
+  },
+  
+  isbnDisplay: function (isbn) {
+    console.log('isbn router: ' + isbn);
+  },
+});
+
+var appRouter = new Router();
+
 var SearchView = Backbone.View.extend({
   events : {
     "submit form": "doSearch"
@@ -9,9 +21,12 @@ var SearchView = Backbone.View.extend({
     var isbn = this.$('input[name=isbn]').val();
 
     console.log("doing a search for " + isbn);
+    
+    appRouter.navigate('isbn/' + isbn, {trigger: true});
   },
 });
 
 $(function() {
   var searcher = new SearchView({el: $('#search-box')});
+  Backbone.history.start();
 });
