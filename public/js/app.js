3fe308beee5692b397f917b09d144cf92bebdb89
Edmund von der Burg <evdb@mysociety.org>
2012-12-24 14:54:15 +0000

    Get jshint passing

--- public/js/app.js
+++ public/js/app.js
@@ -1,66 +1,77 @@
-var BookModel = Backbone.Model.extend({
-  urlRoot: "http://api.127.0.0.1.xip.io/api/books/",
-});
+/*global Backbone:false, $:false, _:false */
 
-
-var BookView = Backbone.View.extend({
-  el: function () {
-    return $('#book-view');
-  },
+(function () {
+  "use strict";
   
-  initialize: function() {
-    this.listenTo(this.model, "change", this.render);
-  },
-
-  render: function () {
-    console.log('BookView render');
-
-    console.log( this.model.url() );
-
-    var data = this.model ? this.model.toJSON() : {};
-
-    var content = _.template( $('#book-view-template').text(), data );
-    this.$el.html( content );
-
-    return this;
-  }
-});
-
-var Router = Backbone.Router.extend({
-  routes: {
-    'isbn/:isbn': 'isbnDisplay',
-  },
+  var BookModel = Backbone.Model.extend({
+    urlRoot: "http://api.127.0.0.1.xip.io/api/books/"
+  });
   
-  isbnDisplay: function (isbn) {
-    console.log('isbn router: ' + isbn);
-
-    var model = new BookModel({ id: isbn });
-
-    var view = new BookView({ model: model });
-    view.render();
-    model.fetch();
-  },
-});
-
-var appRouter = new Router();
-
-var SearchView = Backbone.View.extend({
-  events : {
-    "submit form": "doSearch"
-  },
   
-  doSearch: function (e) {
-    e.preventDefault();
-
-    var isbn = this.$('input[name=isbn]').val();
-
-    console.log("doing a search for " + isbn);
+  var BookView = Backbone.View.extend({
+    el: function () {
+      return $("#book-view");
+    },
     
-    appRouter.navigate('isbn/' + isbn, {trigger: true});
-  },
-});
+    initialize: function () {
+      this.listenTo(this.model, "change", this.render);
+    },
+  
+    render: function () {
 
-$(function() {
-  var searcher = new SearchView({el: $('#search-box')});
-  Backbone.history.start();
-});
+      console.log("BookView render");
+      console.log(this.model.url());
+  
+      var data = this.model ? this.model.toJSON() : {};
+  
+      var content = _.template(
+        $("#book-view-template").text(),
+        data
+      );
+      this.$el.html(content);
+  
+      return this;
+    }
+  });
+  
+  var Router = Backbone.Router.extend({
+    routes: {
+      "isbn/:isbn": "isbnDisplay"
+    },
+    
+    isbnDisplay: function (isbn) {
+      console.log("isbn router: " + isbn);
+  
+      var model = new BookModel({ id: isbn });
+  
+      var view = new BookView({ model: model });
+      view.render();
+      model.fetch();
+    }
+  });
+  
+  var appRouter = new Router();
+  
+  var SearchView = Backbone.View.extend({
+    events : {
+      "submit form": "doSearch"
+    },
+    
+    doSearch: function (e) {
+      e.preventDefault();
+  
+      var isbn = this.$("input[name=isbn]").val();
+  
+      console.log("doing a search for " + isbn);
+      
+      appRouter.navigate("isbn/" + isbn, {trigger: true});
+    }
+  });
+  
+  $(function () {
+    new SearchView({el: $("#search-box")});
+    Backbone.history.start();
+  });
+
+})();
+
