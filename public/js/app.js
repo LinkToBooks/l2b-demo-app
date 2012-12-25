d677783aa393c1757afaa03835fd4889a6fc9a5c
Edmund von der Burg <evdb@mysociety.org>
2012-12-25 00:08:34 +0000

    Create a layout, move the search box and book box into it

--- public/js/app.js
+++ public/js/app.js
@@ -9,18 +9,37 @@
     Backbone.history.start();
   });
   
+  var Layout = Backbone.Marionette.Layout.extend({
+    
+    el: "#content",
+    
+    template: "#layout-template",
+  
+    regions: {
+      searchBox: "#search-box",
+      bookBox:   "#book-box"
+    }
+    
+  });
+  
+  L2BDemoApp.addInitializer(function (options) {
+    L2BDemoApp.layout = new Layout();
+    L2BDemoApp.layout.render();
+  });
+  
   var BookModel = Backbone.Model.extend({
-    urlRoot: "http://api.127.0.0.1.xip.io:3000/books/"
+    urlRoot: "http://api.127.0.0.1.xip.io:3000/books/",
+    defaults: {
+      title: '',
+      isbn: '',
+      author: ''
+    }
   });
   
   
   var BookView = Backbone.Marionette.ItemView.extend({
-    template: "#book-view-template",
+    template: "#book-box-template",
 
-    el: function () {
-      return $("#book-view");
-    },
-    
     initialize: function () {
       this.listenTo(this.model, "change", this.render);
     }
@@ -34,15 +53,19 @@
     isbnDisplay: function (isbn) {
 
       var model = new BookModel({ id: isbn });
-      var view = new BookView({ model: model });
+      var view  = new BookView({ model: model });
+      L2BDemoApp.layout.bookBox.show(view);
   
       model.fetch();
+
     }
   });
   
   var appRouter = new Router();
   
-  var SearchView = Backbone.View.extend({
+  var SearchView = Backbone.Marionette.ItemView.extend({
+    template: "#search-box-template",
+
     events : {
       "submit form": "doSearch"
     },
@@ -55,9 +78,14 @@
       appRouter.navigate("isbn/" + isbn, {trigger: true});
     }
   });
+
+  L2BDemoApp.addInitializer( function (options) {
+    var view = new SearchView();
+    L2BDemoApp.layout.searchBox.show( view );
+    view.render();
+  });
   
   $(function () {
-    new SearchView({el: $("#search-box")});
     L2BDemoApp.start();
   });
 
